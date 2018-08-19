1fa920fd2b4a472f9d0043bfacc37af3de0f01ba
James D. Bartlett III <jimmyb@gmail.com>
2018-08-19 06:56:03 -0500

    server root bug in sw

--- sw.js
+++ sw.js
@@ -1,40 +1,40 @@
-var staticCache = "rest-rev-v0.2";
+var staticCache = "rest-rev-v0.3";
 
 let restCSS = [
-    '/css/styles.css'
+    'css/styles.css'
 ]
 
 let restDATA = [
-    '/data/restaurants.json'
+    'data/restaurants.json'
 ]
 
 let restHTML = [
-    '/index.html',
-    '/restaurant.html',
-	'/restaurant.html?id=1',
-	'/restaurant.html?id=2',
-	'/restaurant.html?id=3',
-	'/restaurant.html?id=4',
-	'/restaurant.html?id=5',
-	'/restaurant.html?id=6',
-	'/restaurant.html?id=7',
-	'/restaurant.html?id=8',
-	'/restaurant.html?id=9',
-	'/restaurant.html?id=10'
+    'index.html',
+    'restaurant.html',
+	'restaurant.html?id=1',
+	'restaurant.html?id=2',
+	'restaurant.html?id=3',
+	'restaurant.html?id=4',
+	'restaurant.html?id=5',
+	'restaurant.html?id=6',
+	'restaurant.html?id=7',
+	'restaurant.html?id=8',
+	'restaurant.html?id=9',
+	'restaurant.html?id=10'
 ]
 
 let restIMG = [
-    '/img/1.jpg', '/img/10.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg',
-    '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg'
+    'img/1.jpg', 'img/10.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg',
+    'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg'
 ];
 
 let restJS = [
-    '/js/dbhelper.js',
-    /* '/private/mapbox_api_key.js', */
-	'/js/key.js',
-    '/js/main.js',
-    '/js/register-sw.js',
-    '/js/restaurant_info.js'
+    'js/dbhelper.js',
+    /* 'private/mapbox_api_key.js', */
+	'js/key.js',
+    'js/main.js',
+    'js/register-sw.js',
+    'js/restaurant_info.js'
 ];
 
 
